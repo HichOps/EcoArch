b5990fa6649a6ca38e7cc6c2814d5bb11eeee8a3
Hichops <hicham.mak86@gmail.com>
2026-01-20 12:45:23 +0100

    feat: define GCP instance resource to enable cost analysis

--- infra/main.tf
+++ infra/main.tf
@@ -8,3 +8,23 @@ terraform {
     prefix = "" # Sera injecté par GitLab
   }
 }
+
+# --- Ressources à créer ---
+
+resource "google_compute_instance" "ecoarch_vm" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = var.boot_disk_image
+      size  = var.boot_disk_size
+    }
+  }
+
+  network_interface {
+    network = "default"
+    # On peut ajouter access_config {} ici si on veut une IP publique
+  }
+}
